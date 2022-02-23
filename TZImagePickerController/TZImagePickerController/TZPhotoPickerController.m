0368a073e1f431f88374be290103b4ce24f1d871
tao.yang <tao.yang@funi365.com>
2022-02-23 23:56:47 +0800

    fix: 修复iOS15相册无法滚动到底部

--- TZImagePickerController/TZImagePickerController/TZPhotoPickerController.m
+++ TZImagePickerController/TZImagePickerController/TZPhotoPickerController.m
@@ -853,9 +853,9 @@ static CGFloat itemMargin = 5;
     if (_shouldScrollToBottom && _models.count > 0) {
         // try fix #1562：https://github.com/banchichen/TZImagePickerController/issues/1562
         if (@available(iOS 15.0, *)) {
-            [_collectionView performBatchUpdates:^{} completion:^(BOOL finished) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.03 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [self scrollCollectionViewToBottom];
-            }];
+            });
         } else {
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [self scrollCollectionViewToBottom];
